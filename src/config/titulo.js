1d0f7d55b9bc21fb1133242939cf69ee65753d0b
Va-Gomez <cmgomez72@misena.edu.co>
2022-10-04 17:00:41 -0500

    Actualizando

--- src/config/titulo.js
+++ src/config/titulo.js
@@ -1,1 +1,1 @@
-module.exports = 'NOMBRE DEL COMPONENTE FORMATIVO'
+module.exports = 'Conectando al turista con la experiencia.'
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,6 +8,14 @@
         span
           i.fas.fa-info
       h1 Introducción
+
+    p.mb-5 Para dar inicio al componente formativo, observe el siguiente video, el cual relaciona los elementos a tener en cuenta en el desarrollo de los conceptos:
+    
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Leyenda del video
+    
     
 </template>
 
